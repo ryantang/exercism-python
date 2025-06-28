581de410dab1990a3814bbace6faec678cbd9139
Ryan Tang <ryantang@gmail.com>
2025-06-28 08:32:19 -0700

    Completed resistor color duo exercise

---
+++ resistor-color-duo/resistor_color_duo.py
@@ -0,0 +1,17 @@
+COLORS = {
+    'black': 0,
+    'brown': 1,
+    'red': 2,
+    'orange': 3,
+    'yellow': 4,
+    'green': 5,
+    'blue': 6,
+    'violet': 7,
+    'grey': 8,
+    'white': 9,
+    }
+
+
+def value(colors):
+    first, second, *_ = colors
+    return COLORS[first] * 10 + COLORS[second]
