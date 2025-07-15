f9150ede7576af0203b7f166dff083c678f0326a
Ryan Tang <ryantang@gmail.com>
2025-07-15 15:44:11 -0700

    Completed protein translation exercise

---
+++ protein-translation/protein_translation.py
@@ -0,0 +1,32 @@
+CODON_MAP = {
+    'AUG': 'Methionine',
+    'UUU': 'Phenylalanine',
+    'UUC': 'Phenylalanine',
+    'UUA': 'Leucine',
+    'UUG': 'Leucine',
+    'UCU': 'Serine',
+    'UCC': 'Serine',
+    'UCA': 'Serine',
+    'UCG': 'Serine',
+    'UAU': 'Tyrosine',
+    'UAC': 'Tyrosine',
+    'UGU': 'Cysteine',
+    'UGC': 'Cysteine',
+    'UGG': 'Tryptophan',
+    'UAA': 'STOP',
+    'UAG': 'STOP',
+    'UGA': 'STOP'
+}
+
+
+def proteins(strand):
+    codons = []
+    for i in range(0, len(strand), 3):
+        triplet = strand[i:i+3]
+        if CODON_MAP[triplet] == 'STOP':
+            break
+        else:
+            codons.append(CODON_MAP[triplet])
+
+    return codons
+
