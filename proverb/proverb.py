2159efde86cbbc03bf7da34765a3bcdac03c6961
ryantang <ryan.tang@quizlet.com>
2025-11-23 07:16:30 -0800

    refactored proverb.py

--- proverb/proverb.py
+++ proverb/proverb.py
@@ -1,24 +1,15 @@
-def proverb(*data, qualifier):
-
-    if len(data) == 0:
+def proverb(*data, qualifier=None):
+    if not data:
         return []
     
     text = []
-    first_item = ""
-    previous = ""
-    for i, item in enumerate(data):
-        if i == 0:
-            first_item = item
-            previous = item
-        else:
-            line = f"For want of a {previous} the {item} was lost."
-            text.append(line)
-            previous = item
-    
-    if qualifier == None:
-        text.append(f"And all for the want of a {first_item}.")
+    for i in range(len(data) - 1):
+        text.append(f"For want of a {data[i]} the {data[i+1]} was lost.")
+
+    if qualifier is None:
+        text.append(f"And all for the want of a {data[0]}.")
     else:
-        text.append(f"And all for the want of a {qualifier} {first_item}.")
+        text.append(f"And all for the want of a {qualifier} {data[0]}.")
 
     return text
 
