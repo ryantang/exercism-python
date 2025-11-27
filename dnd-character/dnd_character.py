d178068b858cc80715f39c2a12cf8c8d46986846
ryantang <ryan.tang@quizlet.com>
2025-11-27 06:43:59 -0800

    refactored dnd_character; fixed bug in dropping largest

--- dnd-character/dnd_character.py
+++ dnd-character/dnd_character.py
@@ -12,30 +12,11 @@ class Character:
 
     @staticmethod
     def ability():
-        rolls = [random.randint(1,6) for _ in range(5)]
+        rolls = [random.randint(1,6) for _ in range(4)]
         rolls.remove(min(rolls))
-        rolls.remove(max(rolls))
         return sum(rolls)
 
 
 def modifier(value):
-    match value:
-        case 3:
-            return -4
-        case 4 | 5:
-            return -3
-        case 6 | 7:
-            return -2
-        case 8 | 9:
-            return -1
-        case 10 | 11:
-            return 0
-        case 12 | 13:
-            return 1
-        case 14 | 15:
-            return 2
-        case 16 | 17:
-            return 3
-        case 18:
-            return 4
+    return (value // 2) - 5
         
