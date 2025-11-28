978e154053232789a29f25c29ad8d769c00ed2df
ryantang <ryan.tang@quizlet.com>
2025-11-28 08:22:42 -0800

    Refactored robot_name
    Used class variable to store the set of used names, because robot names should be unique across all robots. I was previously using just an instance variable.
    
    Used @property to make "def name(self)" a getter. Also renamed internal variable from self.name to self._name to avoid the python gotcha.
    
    Extracted _unique_name method and made it a class method that can be used across all robots which should be unique.

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,24 +1,30 @@
-import random, string
+import random
+import string
 
 class Robot:
-    def __init__(self):
-          self.used_names = set()
-          self.name = self._generate_name()
-          self.used_names.add(self.name)
+    _used_names = set()
 
+    def __init__(self):
+        self._name = Robot._unique_name()
+
+    @property
     def name(self):
-        return self.name
+        return self._name
     
     #factory reset wipes the old name and creates a new one
-    def reset(self):
-        new_name = self._generate_name()
-
-        while new_name in self.used_names:
-            new_name = self._generate_name()
-        
-        self.name = new_name
-        self.used_names.add(new_name)
+    def reset(self):        
+        self._name = Robot._unique_name()
     
+    @classmethod
+    def _unique_name(cls):
+        new_name = cls._generate_name()
+
+        while new_name in cls._used_names:
+            new_name = cls._generate_name()
+        
+        cls._used_names.add(new_name)
+        return new_name
+
     @staticmethod
     def _generate_name():
         prefix = ''.join(random.choice(string.ascii_uppercase) for _ in range(2))
