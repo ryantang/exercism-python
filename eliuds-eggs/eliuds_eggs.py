6338cceba74738d7a2e56422facfcf108966c7f1
ryantang <ryan.tang@quizlet.com>
2025-12-14 07:31:23 -0800

    naming refactor in eliuds_eggs

--- eliuds-eggs/eliuds_eggs.py
+++ eliuds-eggs/eliuds_eggs.py
@@ -2,10 +2,10 @@ import math
 
 def egg_count(display_value):
     eggs = 0
-    number = display_value
+    remaining = display_value
 
-    while number > 0:
-        eggs += number % 2
-        number = number//2
+    while remaining > 0:
+        eggs += remaining % 2
+        remaining = remaining//2
 
     return eggs
