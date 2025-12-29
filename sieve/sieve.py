ad226be980f849c4afc96a88c62f30ea8d362b85
ryantang <ryan.tang@quizlet.com>
2025-12-29 07:57:36 -0800

    Refactored siev.py
    * more descriptive variables
    * used range function to generate multiples
    * pulled out descriptive helper method for zero and one
    * optimized by only counting up to sqrt(limit) and not marking multiples of compisites.

--- sieve/sieve.py
+++ sieve/sieve.py
@@ -1,11 +1,24 @@
-def primes(limit):
-    number_list = [False] * 2 + [True] * (limit - 1)
-    for i in range(2,limit + 1):
-        j = 2
-        while i * j <= limit:
-            print(f'i*j is {i*j}')
-            number_list[i*j] = False
-            j = j+1
+import math
 
-    print(f'number_list is {number_list}')
-    return [index for index, value in enumerate(number_list) if value]
+def primes(limit: int) -> list[int]:
+    """Find primes up to a limit using the Sieve of Eratosthenes algorithm
+
+    Args:
+        limit (int): The upper bound.
+
+    Returns:
+        list: The resulting prime.
+    """
+    prime_by_index = [True] * (limit + 1)
+    _set_zero_and_one_not_prime(prime_by_index)
+
+    for index in range(2, math.floor(math.sqrt(limit)) + 1):
+        if prime_by_index[index]:
+            # Skip composites; their multiples were already marked by smaller primes
+            for multiple in range(index * 2, limit + 1, index):
+                prime_by_index[multiple] = False
+
+    return [index for index, is_prime in enumerate(prime_by_index) if is_prime]
+
+def _set_zero_and_one_not_prime(primes_list) -> None:
+    primes_list[0] = primes_list[1] = False
