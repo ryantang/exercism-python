005d901b0bf77376aca3c3d4949a1ee4088a9076
Ryan Tang <ryantang@gmail.com>
2025-12-31 14:58:05 -0800

    Refactored circular buffer
    Added type hints and DRY'ed out the code a bit.

--- circular-buffer/circular_buffer.py
+++ circular-buffer/circular_buffer.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 class BufferFullException(BufferError):
     """Exception raised when CircularBuffer is full.
 
@@ -5,7 +7,7 @@ class BufferFullException(BufferError):
 
     """
     def __init__(self, message):
-        pass
+        self.message = message
 
 
 class BufferEmptyException(BufferError):
@@ -19,41 +21,38 @@ class BufferEmptyException(BufferError):
 
 
 class CircularBuffer:
-    def __init__(self, capacity):
+    def __init__(self, capacity: int):
         self._contents = [None] * capacity
         self._read_pointer = 0
         self._write_pointer = 0
         self._capacity = capacity
 
-    def read(self):
-        if not self._contents[self._read_pointer]:
+    def read(self) -> Any:
+        if self._contents[self._read_pointer] is None:
             raise BufferEmptyException("Circular buffer is empty")
         
         return_value = self._contents[self._read_pointer]
         self._contents[self._read_pointer] = None
-        self._increment_read_pointer()
+        self._read_pointer = self._advance_pointer(self._read_pointer)
 
         return return_value
 
-    def write(self, data):
-        if self._contents[self._write_pointer]:
+    def write(self, data: Any) -> None:
+        if self._contents[self._write_pointer] is not None:
             raise BufferFullException("Circular buffer is full")
 
         self._contents[self._write_pointer] = data
-        self._increment_write_pointer()
+        self._write_pointer = self._advance_pointer(self._write_pointer)
 
-    def overwrite(self, data):
-        if self._contents[self._write_pointer]:
-            self._increment_read_pointer()
+    def overwrite(self, data: Any) -> None:
+        if self._contents[self._write_pointer] is not None:
+            self._read_pointer = self._advance_pointer(self._read_pointer)
 
         self._contents[self._write_pointer] = data
-        self._increment_write_pointer()
+        self._write_pointer = self._advance_pointer(self._write_pointer)
 
-    def clear(self):
+    def clear(self) -> None:
         self._contents = [None] * self._capacity
     
-    def _increment_write_pointer(self):
-        self._write_pointer = (self._write_pointer + 1) % self._capacity
-    
-    def _increment_read_pointer(self):
-        self._read_pointer = (self._read_pointer + 1) % self._capacity
+    def _advance_pointer(self, pointer: int) -> int:
+        return (pointer + 1) % self._capacity
