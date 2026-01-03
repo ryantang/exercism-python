0f5e0fc9681753dda4dc5fab7c5eeedc56ffd4b7
ryantang <ryan.tang@quizlet.com>
2026-01-03 08:20:07 -0800

    Added docstrings

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -1,4 +1,6 @@
 class Garden:
+    """A kindergarten garden with two rows of plants."""
+
     PLANT_NAMES = {
         "R": "Radishes",
         "C": "Clover",
@@ -21,7 +23,8 @@ class Garden:
         "Larry",
     ]
 
-    def __init__(self, diagram, students=DEFAULT_STUDENTS):
+    def __init__(self, diagram, students=DEFAULT_STUDENTS) -> None:
+        """Initialize garden with plant diagram and student list."""
         self._plants = [
             [self.PLANT_NAMES[encoding] for encoding in row]
             for row in diagram.split("\n")
@@ -30,5 +33,6 @@ class Garden:
         self._students = sorted(students)
 
     def plants(self, student) -> list[str]:
+        """Return the plants belonging to the given student."""
         i = self._students.index(student) * 2
         return self._plants[0][i:i+2] + self._plants[1][i:i+2]
