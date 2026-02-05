bbe5e76feb0e3fa57ce08e591b35fe921d270b0a
ryantang <ryan.tang@quizlet.com>
2026-02-04 21:53:56 -0800

    game of life iteration 2

--- game-of-life/game_of_life.py
+++ game-of-life/game_of_life.py
@@ -1,34 +1,31 @@
 def tick(matrix):
     if matrix == []:
-        return matrix
+        return []
 
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-
-    next_matrix = [[0] * num_cols for _ in range(num_rows)]
+    num_rows, num_col = len(matrix), len(matrix[0])
+    new_matrix = [[0] * num_col for _ in range(num_rows)]
 
     for row_index, row in enumerate(matrix):
-        for column_index, cell in enumerate(row):
-            if cell == 1 and _live_neighbors((row_index,column_index), matrix) in (2, 3):
-                next_matrix[row_index][column_index] = 1
-            elif cell == 0 and _live_neighbors((row_index,column_index), matrix) == 3:
-                next_matrix[row_index][column_index] = 1
+        for col_index, cell in enumerate(row):
+            if (cell == 1 and _live_neighbors((row_index, col_index), matrix) in (2,3)
+                or cell == 0 and _live_neighbors((row_index, col_index), matrix) == 3):
+                new_matrix[row_index][col_index] = 1
 
-    return next_matrix
+    return new_matrix
 
 def _live_neighbors(coordinates, matrix):
-    row_index, column_index = coordinates
+    row_index, col_index = coordinates
+    num_rows, num_col = len(matrix), len(matrix[0])
+
     neighbors = []
 
-    num_rows = len(matrix)
-    num_cols = len(matrix[0])
-
     for i in (-1, 0, 1):
-        y = row_index + i
-        if 0 <= y < num_rows :
-            for j in (-1, 0, 1):
-                x = column_index + j
-                if 0 <= x < num_cols and (i, j) != (0,0):
-                    neighbors.append((y, x))
+        for j in (-1, 0, 1):
+            if (i, j) != (0, 0):
+                neighbor_row = row_index + i
+                neighbor_col = col_index + j
+                if (0 <= neighbor_row < num_rows
+                    and 0 <= neighbor_col < num_col):
+                    neighbors.append((neighbor_row, neighbor_col))
 
-    return sum(matrix[y][x] for y, x in neighbors)
+    return sum(matrix[i][j] for i, j in neighbors)
