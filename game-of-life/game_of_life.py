66f151ff740cce29adbf646224812a9ac2e17bd0
ryantang <ryan.tang@quizlet.com>
2026-02-04 21:24:07 -0800

    Refactored game of life iteration 1
    Generated the neighboring cell list differently

--- game-of-life/game_of_life.py
+++ game-of-life/game_of_life.py
@@ -13,8 +13,6 @@ def tick(matrix):
                 next_matrix[row_index][column_index] = 1
             elif cell == 0 and _live_neighbors((row_index,column_index), matrix) == 3:
                 next_matrix[row_index][column_index] = 1
-            else:
-                next_matrix[row_index][column_index] = 0
 
     return next_matrix
 
@@ -25,18 +23,12 @@ def _live_neighbors(coordinates, matrix):
     num_rows = len(matrix)
     num_cols = len(matrix[0])
 
-    for i in (-1, 1):
+    for i in (-1, 0, 1):
         y = row_index + i
-        if 0 <= y <= num_rows - 1:
+        if 0 <= y < num_rows :
             for j in (-1, 0, 1):
                 x = column_index + j
-                if 0 <= x <= num_cols - 1:
-                    neighbors.append((y,x))
-    for j in (-1, 1):
-        y = row_index
-        x = column_index + j
-        if 0 <= x <= num_cols - 1:
-            neighbors.append((y,x))
+                if 0 <= x < num_cols and (i, j) != (0,0):
+                    neighbors.append((y, x))
 
-    print(f'y ix {y} and x is {x}')
     return sum(matrix[y][x] for y, x in neighbors)
