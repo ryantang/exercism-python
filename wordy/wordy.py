2f97df8f2be7e83193874d5cc5e4eba71e4453cb
ryantang <ryan.tang@quizlet.com>
2026-02-14 07:05:28 -0800

    WIP got all tests to pass in wordy.py

--- wordy/wordy.py
+++ wordy/wordy.py
@@ -1,15 +1,25 @@
 import re
 
 def answer(question):
-    start_match = re.search(r"What is (-?\d+)", question)
+    start_match = re.search(r"What is (-?\d+)([^?]*)?", question)
+    if not start_match:
+        raise ValueError("syntax error")
+
     starting_number = start_match.group(1)
-    print(f'starting number is {starting_number}')
+    remaining_stuff = start_match.group(2)
 
-    operations = re.findall(r"(plus|minus|multiplied by|divided by) (-?\d+)", question)
-    print(f"operations is {operations}")
-
-    if starting_number and not operations:
+    if starting_number and not remaining_stuff:
         return int(starting_number)
+
+    if remaining_stuff.strip() in ("cubed"):
+        raise ValueError("unknown operation")
+
+    structure_pattern = r"^( (?:plus|minus|multiplied by|divided by) -?\d+)+$"
+    if not re.match(structure_pattern, remaining_stuff):
+        raise ValueError("syntax error")
+
+    operations = re.findall(r"(plus|minus|multiplied by|divided by) (-?\d+)", remaining_stuff)
+    print(f"operations is {operations}")
 
     accumulator = int(starting_number)
     for operation in operations:
