f04470c047816b0bf7e5b0002232605ba4677d0a
ryantang <ryan.tang@quizlet.com>
2026-03-07 18:24:45 -0800

    Refactored connect.py
    * Set PLAYER_X and PLAYER_O instead of magic strings
    * Removed redundant guard clause
    * Removed some extra variables

--- connect/connect.py
+++ connect/connect.py
@@ -3,6 +3,9 @@ from collections import namedtuple
 Point = namedtuple('Point', ('row_index', 'col_index'))
 
 class ConnectGame:
+    PLAYER_X = 'X'
+    PLAYER_O = 'O'
+
     def __init__(self, board):
         rows = board.split('\n')
         self.matrix = [
@@ -13,45 +16,39 @@ class ConnectGame:
         self.board_height = len(self.matrix)
         self.board_width = len(self.matrix[0])
 
-        print(f'matrix is {self.matrix}')
-
     def get_winner(self):
-        if self._empty_board():
-            return ''
-
-        x_left_edge = []
-        for row_index, row in enumerate(self.matrix):
-            if row[0] == 'X':
-                x_left_edge.append(Point(row_index=row_index, col_index=0))
+        x_left_edge = (
+            Point(row_index=row_index, col_index=0)
+            for row_index, row in enumerate(self.matrix)
+            if row[0] == self.PLAYER_X
+        )
 
         for start_node in x_left_edge:
-            if self._complete_path('X', [start_node]):
-                return 'X'
+            if self._complete_path(self.PLAYER_X, [start_node]):
+                return self.PLAYER_X
 
-        o_top_edge = []
-        for col_index, node in enumerate(self.matrix[0]):
-            if node == 'O':
-                o_top_edge.append(Point(row_index=0, col_index=col_index))
+        o_top_edge = (
+            Point(row_index=0, col_index=col_index)
+            for col_index, node in enumerate(self.matrix[0])
+            if node == self.PLAYER_O
+        )
 
         for start_node in o_top_edge:
-            if self._complete_path('O', [start_node]):
-                return 'O'
+            if self._complete_path(self.PLAYER_O, [start_node]):
+                return self.PLAYER_O
 
         return ''
 
 
     def _complete_path(self, player, path):
-        if not path:
-            raise ValueError('Function _complete_path requires a starting node')
-
         # Win conditions
         current_node = path[-1]
-        if player == 'X' and current_node.col_index == self.board_width - 1:
+        if player == self.PLAYER_X and current_node.col_index == self.board_width - 1:
             return True
-        if player == 'O' and current_node.row_index == self.board_height - 1:
+        if player == self.PLAYER_O and current_node.row_index == self.board_height - 1:
             return True
 
-        possible_next_nodes = self._neighbors(path[-1], player) - set(path)
+        possible_next_nodes = self._neighbors(current_node, player) - set(path)
         for node in possible_next_nodes:
             if self._complete_path(player, path + [node]):
                 return True
@@ -70,25 +67,16 @@ class ConnectGame:
 
         neighbors = set()
         for delta in deltas:
-            row_index = node.row_index + delta.row_index
-            col_index = node.col_index + delta.col_index
-
-            candidate = Point(row_index, col_index)
-
-            if self._on_board(candidate):
-                if self.matrix[row_index][col_index] == player:
-                    neighbors.add(candidate)
+            candidate = Point(
+                row_index = node.row_index + delta.row_index,
+                col_index = node.col_index + delta.col_index
+            )
+            if (self._on_board(candidate)
+                    and self.matrix[candidate.row_index][candidate.col_index] == player):
+                neighbors.add(candidate)
 
         return neighbors
 
     def _on_board(self, node):
         return  (0 <= node.row_index < self.board_height
                     and 0 <= node.col_index < self.board_width)
-
-
-    def _empty_board(self):
-        return all(
-            cell == '.'
-            for row in self.matrix
-            for cell in row
-        )
