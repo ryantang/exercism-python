b954dee5a10e2584420cfcacce5c69bba5f46d1e
ryantang <ryan.tang@quizlet.com>
2026-03-28 16:00:37 -0700

    Reafactored spiral_matrix
    Use itertools.cycle instead of my custom turn_right() function.
    Add type hints and doc strings.

--- spiral-matrix/spiral_matrix.py
+++ spiral-matrix/spiral_matrix.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from itertools import cycle
 
 Cell = namedtuple('Cell', ['row_index', 'col_index'])
 Delta = namedtuple('Delta', ['row_delta', 'col_delta'])
@@ -10,13 +11,15 @@ UP = Delta(-1, 0)
 DIRECTIONS = (RIGHT, DOWN, LEFT, UP)
 
 def spiral_matrix(size: int) -> list[list[int]]:
+    """Creates a spiral matrix of a given size."""
     matrix = [
         [None] * size
         for _ in range(size)
     ]
 
     cell = Cell(0, 0)
-    direction = RIGHT
+    direction_cycler = cycle(DIRECTIONS)
+    direction = next(direction_cycler)
 
     for i in range(size ** 2):
         matrix[cell.row_index][cell.col_index] = i + 1
@@ -26,19 +29,17 @@ def spiral_matrix(size: int) -> list[list[int]]:
             matrix[forward_cell.row_index][forward_cell.col_index] is None):
             cell = forward_cell
         else:
-            direction = _turn_right(direction)
+            direction = next(direction_cycler)
             cell = _forward_cell(cell, direction)
 
     return matrix
 
-def _in_boundary(matrix_size: int, cell):
+def _in_boundary(matrix_size: int, cell: Cell) -> bool:
+    """Checks if a cell is within the matrix boundaries."""
     return cell.row_index < matrix_size and cell.col_index < matrix_size
 
-def _turn_right(direction):
-    index = (DIRECTIONS.index(direction) + 1) % len(DIRECTIONS)
-    return DIRECTIONS[index]
-
-def _forward_cell(cell, direction):
+def _forward_cell(cell: Cell, direction: Delta) -> Cell:
+    """Calculates the next cell's position based on the current direction."""
     return Cell(
             cell.row_index + direction.row_delta,
             cell.col_index + direction.col_delta
