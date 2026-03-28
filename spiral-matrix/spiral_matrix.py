58da63d3cf00c363873d4b1ab70e31e177333522
ryantang <ryan.tang@quizlet.com>
2026-03-28 11:49:17 -0700

    Refactored spiral_matrix
    Using a for loop because we know the exact number of cells is a key insight to make this cleaner.

--- spiral-matrix/spiral_matrix.py
+++ spiral-matrix/spiral_matrix.py
@@ -1,66 +1,45 @@
 from collections import namedtuple
 
-RIGHT = (0, 1)
-DOWN = (1, 0)
-LEFT = (0, -1)
-UP = (-1, 0)
+Cell = namedtuple('Cell', ['row_index', 'col_index'])
+Delta = namedtuple('Delta', ['row_delta', 'col_delta'])
 
+RIGHT = Delta(0, 1)
+DOWN = Delta(1, 0)
+LEFT = Delta(0, -1)
+UP = Delta(-1, 0)
 DIRECTIONS = (RIGHT, DOWN, LEFT, UP)
-Cell = namedtuple('Cell', ('row_index', 'col_index'))
 
-def spiral_matrix(size):
+def spiral_matrix(size: int) -> list[list[int]]:
     matrix = [
         [None] * size
         for _ in range(size)
     ]
 
-    current_cell = Cell(0, 0)
-    step = 1
+    cell = Cell(0, 0)
     direction = RIGHT
 
-    while _within_matrix(matrix, current_cell) and _cell_empty(matrix, current_cell):
-        current_cell, step = _fill_to_end(matrix, current_cell, step, direction)
-        direction = _turn_right(direction)
-        current_cell = _next_cell(current_cell, direction)
+    for i in range(size ** 2):
+        matrix[cell.row_index][cell.col_index] = i + 1
+        forward_cell = _forward_cell(cell, direction)
+
+        if (_in_boundary(size, forward_cell) and
+            matrix[forward_cell.row_index][forward_cell.col_index] is None):
+            cell = forward_cell
+        else:
+            direction = _turn_right(direction)
+            cell = _forward_cell(cell, direction)
 
     return matrix
 
+def _in_boundary(matrix_size: int, cell):
+    return cell.row_index < matrix_size and cell.col_index < matrix_size
+
 def _turn_right(direction):
-    index = DIRECTIONS.index(direction)
-    new_index = (index + 1) % len(DIRECTIONS)
-    return DIRECTIONS[new_index]
+    index = (DIRECTIONS.index(direction) + 1) % len(DIRECTIONS)
+    return DIRECTIONS[index]
 
-
-def _fill_to_end(matrix, current_cell, step, direction):
-    if not _cell_empty(matrix, current_cell):
-        return (current_cell, step)
-
-    matrix[current_cell.row_index][current_cell.col_index] = step
-    step += 1
-
-    next_cell = _next_cell(current_cell, direction)
-
-    if not _within_matrix(matrix, next_cell) or not _cell_empty(matrix, next_cell):
-        return (current_cell, step)
-
-    current_cell = next_cell
-    print(f'current_cell is {current_cell}')
-    print(f'matrix is {matrix}\n')
-
-    return _fill_to_end(matrix, current_cell, step, direction)
-
-
-def _cell_empty(matrix: list[list[int|None]], cell: tuple[int, int]) -> bool:
-    return matrix[cell.row_index][cell.col_index] is None
-
-def _within_matrix(matrix, cell) -> bool:
-    size = len(matrix)
-    return cell.row_index < size and cell.col_index < size
-
-def _next_cell(current_cell, direction):
-    potential_next_cell = Cell(
-        row_index = current_cell.row_index + direction[0],
-        col_index = current_cell.col_index + direction[1]
+def _forward_cell(cell, direction):
+    return Cell(
+            cell.row_index + direction.row_delta,
+            cell.col_index + direction.col_delta
     )
-
-    return potential_next_cell
