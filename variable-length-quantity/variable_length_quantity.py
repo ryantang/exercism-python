a6da8a578610370d7479032d7393f1ff49be2cbb
ryantang <ryan.tang@quizlet.com>
2026-04-02 09:03:54 -0700

    Refactor variable_length_quantity by removing magic numbers.

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -1,20 +1,24 @@
 from collections import deque
+SEVEN_BIT_BASE = 128
+CONTINUATION_BIT = 128
 
 def encode(numbers: list[int]) -> list[int]:
     """Encodes a list of numbers into a variable-length quantity."""
     encoded_all = []
-    for value in numbers:
+    for number in numbers:
         encoded = deque()
-        if value == 0:
+        remaining_value = number
+
+        if remaining_value == 0:
             encoded.append(0)
 
-        while value:
-            hex_digit = value % 128
-            value = value // 128
+        while remaining_value:
+            hex_digit = remaining_value % SEVEN_BIT_BASE
+            remaining_value = remaining_value // SEVEN_BIT_BASE
 
             # Set "continuation" bit for hex digits after the first hex digit.
             if encoded:
-                hex_digit += 128
+                hex_digit += CONTINUATION_BIT
             encoded.appendleft(hex_digit)
 
         encoded_all += encoded
@@ -31,7 +35,7 @@ def decode(bytes_: list[int]) -> list[int]:
     segment = []
     for hex_digit in bytes_:
         if _continuation_bit_set(hex_digit):
-            segment.append(hex_digit % 128)
+            segment.append(hex_digit % CONTINUATION_BIT)
         else:
             segment.append(hex_digit)
             decoded.append(_decode_segment(segment))
@@ -41,12 +45,12 @@ def decode(bytes_: list[int]) -> list[int]:
 
 def _continuation_bit_set(hex_digit: int) -> bool:
     """Checks if the continuation bit is set for a given byte."""
-    return bool(hex_digit // 128)
+    return bool(hex_digit // CONTINUATION_BIT)
 
 def _decode_segment(bytes_: list[int]) -> int:
     """Decodes a single variable-length number segment."""
     decoded = 0
     for place, hex_value in enumerate(reversed(bytes_)):
-        decoded += (hex_value % 128) * 128 ** place
+        decoded += (hex_value % SEVEN_BIT_BASE) * SEVEN_BIT_BASE ** place
 
     return decoded
