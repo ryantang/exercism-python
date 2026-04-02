affec7b6c115592fa2c52a0232629fea17771da7
ryantang <ryan.tang@quizlet.com>
2026-04-02 08:42:51 -0700

    Refactored variable_lenghth_quantity
    * Used deque instead of reversing a list.
    * Added type hints
    * Added doc strings

--- variable-length-quantity/variable_length_quantity.py
+++ variable-length-quantity/variable_length_quantity.py
@@ -1,7 +1,10 @@
-def encode(numbers):
+from collections import deque
+
+def encode(numbers: list[int]) -> list[int]:
+    """Encodes a list of numbers into a variable-length quantity."""
     encoded_all = []
     for value in numbers:
-        encoded = []
+        encoded = deque()
         if value == 0:
             encoded.append(0)
 
@@ -9,41 +12,41 @@ def encode(numbers):
             hex_digit = value % 128
             value = value // 128
 
-            # Flip "continuation" bit for hex digits after the first hex digit.
+            # Set "continuation" bit for hex digits after the first hex digit.
             if encoded:
                 hex_digit += 128
-            encoded.append(hex_digit)
+            encoded.appendleft(hex_digit)
 
-        encoded_all += list(reversed(encoded))
+        encoded_all += encoded
 
     return encoded_all
 
 
-def decode(bytes_):
+def decode(bytes_: list[int]) -> list[int]:
+    """Decodes a variable-length quantity into a list of numbers."""
+    if not bytes_ or _continuation_bit_set(bytes_[-1]):
+        raise ValueError("incomplete sequence")
+
     decoded = []
     segment = []
-    for place, hex_digit in enumerate(bytes_):
-        if place == len(bytes_) - 1 and _continuation_bit_set(hex_digit):
-            raise ValueError("incomplete sequence")
-
+    for hex_digit in bytes_:
         if _continuation_bit_set(hex_digit):
             segment.append(hex_digit % 128)
         else:
             segment.append(hex_digit)
-            decoded += _decode_segment(segment)
+            decoded.append(_decode_segment(segment))
             segment = []
-
-    if segment:
-        decoded += _decode_segment(segment)
 
     return decoded
 
 def _continuation_bit_set(hex_digit: int) -> bool:
+    """Checks if the continuation bit is set for a given byte."""
     return bool(hex_digit // 128)
 
-def _decode_segment(bytes_):
+def _decode_segment(bytes_: list[int]) -> int:
+    """Decodes a single variable-length number segment."""
     decoded = 0
     for place, hex_value in enumerate(reversed(bytes_)):
         decoded += (hex_value % 128) * 128 ** place
 
-    return [decoded]
+    return decoded
