0424f73667171a15eef8ea9587b2944bd8295033
ryantang <ryan.tang@quizlet.com>
2026-05-11 08:22:26 -0700

    Refactored tournament.py
    used f-stings instead of rjustand ljust for cleaner string creation.
    added type hints

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -13,30 +13,23 @@ class Team:
         return 3 * self.wins + self.draws
 
 
-def tally(rows):
+def tally(rows: list[str]) -> list[str]:
     teams = _tabulate_team_records(rows)
     sorted_teams = sorted(teams.values(), key=lambda team: (-team.points, team.name))
 
-    heading = "Team                           | MP |  W |  D |  L |  P"
+    heading = f'{'Team':<30} | MP |  W |  D |  L |  P'
     team_stats = _format_team_stats(sorted_teams)
     return [heading] + team_stats
 
-def _format_team_stats(teams) -> list[str]:
-    all_teams_stats = []
-    for team in teams:
-        team_stats = ' | '.join([
-            team.name.ljust(30),
-            str(team.matches_played).rjust(2),
-            str(team.wins).rjust(2),
-            str(team.draws).rjust(2),
-            str(team.losses).rjust(2),
-            str(team.points).rjust(2),
-        ])
-        all_teams_stats.append(team_stats)
-    return all_teams_stats
+def _format_team_stats(teams: list[str]) -> list[str]:
+    return [
+        f'{team.name:<30} | {team.matches_played:>2} | {team.wins:>2} | '
+        f'{team.draws:>2} | {team.losses:>2} | {team.points:>2}'
+        for team in teams
+    ]
 
 
-def _tabulate_team_records(rows) -> dict[str, Team]:
+def _tabulate_team_records(rows: list[str]) -> dict[str, Team]:
     teams = {}
     for row in rows:
         team_name1, team_name2, result = row.split(';')
@@ -46,7 +39,7 @@ def _tabulate_team_records(rows) -> dict[str, Team]:
         _update_records_from_result(team1, team2, result)
     return teams
 
-def _update_records_from_result(team1, team2, result):
+def _update_records_from_result(team1: Team, team2: Team, result: str):
     team1.matches_played += 1
     team2.matches_played += 1
 
