cfb3e4e879d75a2e809241b8c07c784dc790c09a
ryantang <ryan.tang@quizlet.com>
2026-05-31 21:25:56 -0700

    Refactored scale_generator.py
    * Removed the Notes class
    * Used itertools's accumulate()
    * Added static method
    * Various other cleanups

--- scale-generator/scale_generator.py
+++ scale-generator/scale_generator.py
@@ -1,74 +1,41 @@
+"""Build musical scales from a tonic, as chromatic or interval-based sequences."""
+
+from itertools import accumulate
+
+
 class Scale:
-    RAW_NOTES = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    """Generate chromatic and interval-based scales from a tonic."""
+
+    SHARP = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+    FLAT = ['A', 'Bb', 'B', 'C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab']
+    SHARP_SIG = {'G', 'D', 'A', 'E', 'B', 'F#', 'e', 'b', 'f#', 'c#', 'g#', 'd#'}
+    # Keys that use flats: 'F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'd', 'g', 'c', 'f', 'bb', 'eb'
+    NATURAL_SIG = {'C', 'a'}
     NUM_NOTES = 12
+    INTERVALS = {'m': 1, 'M': 2, 'A': 3}
 
-    def __init__(self, tonic):
-        self._tonic = Note(tonic)
-        self._scale_start = self.RAW_NOTES.index(self._tonic.to_sharp())
-        self._full_chromatic = self._notes()[self._scale_start:] + self._notes()[:self._scale_start + 1]
+    def __init__(self, tonic: str) -> None:
+        self._scale = self.SHARP if tonic in (self.SHARP_SIG | self.NATURAL_SIG) else self.FLAT
+        self._scale_start = self._scale.index(self._normalize(tonic))
+        self._full_chromatic = self._scale[self._scale_start:] + self._scale[:self._scale_start + 1]
 
-    def chromatic(self):
-        return self._full_chromatic[:12]
+    def chromatic(self) -> list[str]:
+        """Return the 12-note chromatic scale starting from the tonic."""
+        return self._full_chromatic[:self.NUM_NOTES]
 
-    def interval(self, intervals):
-        interval_scale = [self._full_chromatic[0]]
-        pointer = 0
-        for step in self._stepify(intervals):
-            pointer += step
-            interval_scale.append(self._full_chromatic[pointer])
+    def interval(self, intervals: list[str]) -> list[str]:
+        """Return the scale's notes selected by interval steps (m/M/A)."""
+        steps = [self.INTERVALS[interval] for interval in intervals]
 
-        return interval_scale
+        return [
+            self._full_chromatic[index]
+            for index in accumulate(steps, initial=0)
+        ]
 
-    def _stepify(self, intervals):
-        steps = []
-        for interval in intervals:
-            if interval == 'M':
-                steps.append(2)
-            if interval == 'm':
-                steps.append(1)
-            if interval == 'A':
-                steps.append(3)
+    @staticmethod
+    def _normalize(note: str) -> str:
+        """Capitalize a note's letter, leaving any accidental intact."""
+        if not 1 <= len(note) <= 2:
+            raise ValueError(f'{note} is not a valid note')
 
-        return steps
-
-
-    def _notes(self):
-        return [Note(note) for note in self.RAW_NOTES]
-
-
-class Note:
-    EQUIVALENTS = {
-        'A#': 'Bb',
-        'C#': 'Db',
-        'D#': 'Eb',
-        'F#': 'Gb',
-        'G#': 'Ab',
-        'Bb': 'A#',
-        'Db': 'C#',
-        'Eb': 'D#',
-        'Gb': 'F#',
-        'Ab': 'G#',
-    }
-
-    def __init__(self, string_note):
-        self.note = self._standardize(string_note)
-
-    def __eq__(self, string_note):
-        return self.note == string_note or self.EQUIVALENTS.get(self.note) == string_note
-
-    def __str__(self):
-        return self.note
-
-    def to_sharp(self):
-        if len(self.note) == 2 and self.note[1] == 'b':
-            return str(self.EQUIVALENTS[self.note])
-        return str(self.note)
-
-    def _standardize(self, string_note):
-        if len(string_note) == 1:
-            return string_note.upper()
-        if len(string_note) == 2:
-            return string_note[0].upper() + string_note[1]
-
-        raise ValueError(f'{string_note} is not a valid note')
-
+        return note[0].upper() + note[1:]
